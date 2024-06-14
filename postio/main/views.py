47ff5777d7499875bb047d495972972547a2dc94
Kamil Handzlik <kamol0209@gmail.com>
2024-06-14 21:20:37 +0200

    set initial base for home site added main to sttings.py and path to home site was made

--- postio/main/views.py
+++ postio/main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    """Wyświetl stronę główną"""
+    return render(request, 'main/base.html', {})
--- postio/postio/settings.py
+++ postio/postio/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
